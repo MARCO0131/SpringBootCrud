16b3b6cb8ebb628345dd6be9df97f275d810b07c
MARCO0131 <riteshbadgotya@gmail.com>
2022-07-22 21:49:58 +0530

    swagger ui and resourses added

--- src/main/java/com/example/boot/controller/BootApplication.java
+++ src/main/java/com/example/boot/controller/BootApplication.java
@@ -4,14 +4,23 @@ package com.example.boot.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication(scanBasePackages = "com.example.boot")
-@EntityScan("com.example.boot.entity")
+@EntityScan(basePackages = "com.example.boot.entity")
 @EnableJpaRepositories(basePackages = "com.example.boot.repository")
 public class BootApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
